1f619d6d46c25011405be3938ff3ca6ad11a3e7f
Alex Roux <lx256043@gmail.com>
2026-07-07 17:35:18 -0500

    Actuator (#3)
    * Coded the Actuator movement.
    
    * Actuator code created.
    
    ---------
    
    Co-authored-by: Lx256043 <lx6043@gmail.com>

---
+++ t-shirt-cannon/src/main/java/frc/robot/subsystems/ValveUp.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class ValveUp extends Command{
+
+    public Actuator m_actuator;
+
+    public ValveUp(Actuator actuator) {
+        m_actuator = actuator;
+    }
+
+    public void execute() {
+        m_actuator.Set(0.5f);
+        System.out.println("Moving up");
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void end(boolean interrupted) {
+        m_actuator.Set(0.0f);
+        System.out.println("Not moving");
+    }
+}
